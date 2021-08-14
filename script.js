9b7640a50e9ad586b6f68aee7b069dab6a86688c
Angie Nguyen <angie.ta.nguyen@gmail.com>
2021-08-14 13:48:38 -0700

    Added typewriting effect to the intro though style.css and added a helper function in script.js

--- script.js
+++ script.js
@@ -52,6 +52,65 @@ function changeIcon(pSelect, cSelect) {
     }
 }
 
+function roundChanges(round, playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        document.querySelector("body").style.background = "linear-gradient(hsl(0, 100%, 98%), white, white,white,hsl(0, 100%, 98%)) no-repeat";
+        document.getElementById("user-score").innerHTML = "Suspicious Human: " + playerScore;
+        document.getElementById("computer-score").innerHTML = "Astounded Computer: " + computerScore;
+        switch(round) {
+            case 2: 
+                document.getElementById("game-remark").innerHTML = "Beginner's luck";
+                break;
+            case 3:
+                document.getElementById("game-remark").innerHTML = "Hmph, looks like I've underestimated you";
+                break;
+            case 4: 
+                document.getElementById("game-remark").innerHTML = "All my work...";
+                break;
+            case 5: 
+                document.getElementById("game-remark").innerHTML = "What kind of sorcery is this?";
+                break;
+        }
+    }
+    else if (playerScore < computerScore)  {
+        document.querySelector("body").style.background = "linear-gradient(hsl(213, 100%, 93%), white, white,white,hsl(213, 100%, 93%)) no-repeat";
+        document.getElementById("user-score").innerHTML = "Foolish Human: " + playerScore;
+        document.getElementById("computer-score").innerHTML = "Brilliant Computer: " + computerScore;
+        switch(round) {
+            case 2: 
+                document.getElementById("game-remark").innerHTML = "This will be a win for machines everywhere";
+                break;
+            case 3:
+                document.getElementById("game-remark").innerHTML = "Don't be sad about your predictability, human";
+                break;
+            case 4: 
+                document.getElementById("game-remark").innerHTML = "Looks like we're getting closer and closer to world domination...";
+                break;
+            case 5: 
+                document.getElementById("game-remark").innerHTML = "I would say I'd keep you as a pet but dogs are better";
+                break;
+        }
+    }
+    else {
+        document.getElementById("user-score").innerHTML = "Human: " + playerScore;
+        document.getElementById("computer-score").innerHTML = "Computer: " + computerScore;
+        switch(round) {
+            case 2: 
+                document.getElementById("game-remark").innerHTML = "Never thought I'd think the same way as a human";
+                break;
+            case 3:
+                document.getElementById("game-remark").innerHTML = "Evenly matched, for now";
+                break;
+            case 4: 
+                document.getElementById("game-remark").innerHTML = "Twinsies! That's what you humans say, right?";
+                break;
+            case 5: 
+                document.getElementById("game-remark").innerHTML = "Great minds think alike";
+                break;
+        }
+    }
+}
+
 let playerSelection;
 let computerSelection;
 let round = 1;
@@ -70,62 +129,7 @@ function game() {
         changeIcon(playerSelection, computerSelection);
         //Change background color and remark depending on how well the player is doing
         //Update scoreboard
-        if (playerScore > computerScore) {
-            document.querySelector("body").style.background = "linear-gradient(hsl(0, 100%, 98%), white, white,white,hsl(0, 100%, 98%)) no-repeat";
-            document.getElementById("user-score").innerHTML = "Suspicious Human: " + playerScore;
-            document.getElementById("computer-score").innerHTML = "Astounded Computer: " + computerScore;
-            switch(round) {
-                case 2: 
-                    document.getElementById("game-remark").innerHTML = "Beginner's luck";
-                    break;
-                case 3:
-                    document.getElementById("game-remark").innerHTML = "Hmph, looks like I've underestimated you";
-                    break;
-                case 4: 
-                    document.getElementById("game-remark").innerHTML = "All my work...";
-                    break;
-                case 5: 
-                    document.getElementById("game-remark").innerHTML = "What kind of sorcery is this?";
-                    break;
-            }
-        }
-        else if (playerScore < computerScore)  {
-            document.querySelector("body").style.background = "linear-gradient(hsl(213, 100%, 93%), white, white,white,hsl(213, 100%, 93%)) no-repeat";
-            document.getElementById("user-score").innerHTML = "Foolish Human: " + playerScore;
-            document.getElementById("computer-score").innerHTML = "Brilliant Computer: " + computerScore;
-            switch(round) {
-                case 2: 
-                    document.getElementById("game-remark").innerHTML = "This will be a win for machines everywhere";
-                    break;
-                case 3:
-                    document.getElementById("game-remark").innerHTML = "Don't be sad about your predictability, human";
-                    break;
-                case 4: 
-                    document.getElementById("game-remark").innerHTML = "Looks like we're getting closer and closer to world domination...";
-                    break;
-                case 5: 
-                    document.getElementById("game-remark").innerHTML = "I would say I'd keep you as a pet but dogs are better";
-                    break;
-            }
-        }
-        else {
-            document.getElementById("user-score").innerHTML = "Human: " + playerScore;
-            document.getElementById("computer-score").innerHTML = "Computer: " + computerScore;
-            switch(round) {
-                case 2: 
-                    document.getElementById("game-remark").innerHTML = "Never thought I'd think the same way as a human";
-                    break;
-                case 3:
-                    document.getElementById("game-remark").innerHTML = "Evenly matched, for now";
-                    break;
-                case 4: 
-                    document.getElementById("game-remark").innerHTML = "Twinsies! That's what you humans say, right?";
-                    break;
-                case 5: 
-                    document.getElementById("game-remark").innerHTML = "Great minds think alike";
-                    break;
-            }
-        }
+        roundChanges(round, playerScore, computerScore);
     }
     //Once five rounds are played, give the appropriate results
     if (round > 5) {
@@ -135,7 +139,7 @@ function game() {
         }
         else if (playerScore < computerScore) {
             document.getElementById("win-lose").innerHTML = "FAILURE";
-            document.getElementById("results-remark").innerHTML = "If only I downloaded myself an audio file for evil laughter";
+            document.getElementById("results-remark").innerHTML = "If I had an evil laugh file, you'd hear it now";
         }
         else {
             document.getElementById("win-lose").innerHTML = "TIE";
